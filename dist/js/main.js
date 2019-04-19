29614fe68f4702f61d49d71933b55205fa79df51
avadhutthorat <avadhutthorat40@gmail.com>
2019-04-19 10:33:49 +0530

    Added menu button and rotation

---
+++ dist/js/main.js
@@ -0,0 +1,37 @@
+// Select DOM elements
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial state of Menu
+
+let showMenu = false;
+
+// On click of menu button
+menuBtn.addEventListener("click", toggleMenu);
+
+// Menu Toggle
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    // set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // set menu state
+    showMenu = true;
+  }
+}
